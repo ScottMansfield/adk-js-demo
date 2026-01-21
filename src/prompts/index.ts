1d0170ccbe15a11c61b5e6bd489019ced6a17fd2
Scott Mansfield <scottmansfield@google.com>
2026-01-20 19:59:04 -0800

    Apply Apache 2.0 License header to all code files

--- src/prompts/index.ts
+++ src/prompts/index.ts
@@ -1,3 +1,9 @@
+/**
+ * @license
+ * Copyright 2025 Google LLC
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 export * from "./contractor_scout.prompt.js";
 export * from "./design_consultant.prompt.js";
 export * from "./material_sourcer.prompt.js";
